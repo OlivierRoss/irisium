0b9efd82686e7ab92c31c83153fd03d587b02c35
Olivier Ross <olivier.ross@usherbrooke.ca>
2017-07-10 01:14:34 +0000

    recherche evenement

--- app/assets/javascripts/evenements.js
+++ app/assets/javascripts/evenements.js
@@ -3,25 +3,19 @@
 
 window.onload = function () {
 
-    // Seulement pour la page evenements 
-    if(window.location.pathname != "/evenements") return;
+    if(window.location.pathname == "/evenements") {
+        document.getElementById("champ-recherche").addEventListener("keyup", filtrer_evenements);
+    }
 
-    document.getElementById("champ-recherche").addEventListener("change", filtrer_evenements);
-    console.log('ici');
 };
 
 function filtrer_evenements () {
-    console.log('la');
+
     var filtre = new RegExp(document.getElementById("champ-recherche").value);
     var evenements = document.getElementsByClassName("evenement");
 
     for(var i = 0; i < evenements.length; i++) {
-        if(filtre.test(evenements[i].textContent)){
-            evenements[i].style.display = "none";
-        }
-        else {
-            evenements[i].style.display = "block";
-        }
+        evenements[i].style.display = filtre.test(evenements[i].textContent) ? "block" : "none";
     }
     
 }
