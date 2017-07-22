80d4b2a931bc4d100e767f728de657974153dd76
Olivier Ross <olivier.ross@usherbrooke.ca>
2017-07-22 03:13:54 +0000

    feature

--- app/helpers/evenements_helper.rb
+++ app/helpers/evenements_helper.rb
@@ -1,5 +1,5 @@
 module EvenementsHelper
     def image_categorie (id)
-        image_tag("/assets/#{Evenement::categories.key(id)}.jpg")
+        image_tag(asset_url(Evenement::categories.key(id)))
     end
 end
