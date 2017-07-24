7f45a61b99f21a84199ae45be220216f2acebdac
Olivier Ross <olivier.ross@usherbrooke.ca>
2017-07-24 16:53:10 +0000

    images categories

--- app/helpers/evenements_helper.rb
+++ app/helpers/evenements_helper.rb
@@ -1,5 +1,9 @@
 module EvenementsHelper
-    def image_categorie (id)
-        image_tag(asset_url(Evenement::categories.key(id)))
+    def image_categorie (id, inverser = false)
+        if inverser
+            image_tag(asset_url(Evenement::categories.key(id) + "_inv"))
+        else
+            image_tag(asset_url(Evenement::categories.key(id)))
+        end
     end
 end
