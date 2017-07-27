60e1c7eb2f8446eed3141ec83a964464eed803c9
Olivier Ross <olivier.ross@usherbrooke.ca>
2017-07-27 02:56:33 +0000

    tooltip

--- app/helpers/evenements_helper.rb
+++ app/helpers/evenements_helper.rb
@@ -1,9 +1,9 @@
 module EvenementsHelper
     def image_categorie (id, inverser = false)
         if inverser
-            image_tag(Evenement::categories.key(id) + "_inv.svg")
+            image_tag(Evenement::categories.key(id) + "_inv.svg", :title => Evenement::categories.key(id), :alt => Evenement::categories.key(id))
         else
-            image_tag(Evenement::categories.key(id) + ".svg")
+            image_tag(Evenement::categories.key(id) + ".svg", :title => Evenement::categories.key(id), :alt => Evenement::categories.key(id))
         end
     end
 end
